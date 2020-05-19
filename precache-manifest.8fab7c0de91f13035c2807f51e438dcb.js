bc934811581fcf1e7f84c5a47c0104ccead2d472
Makos <makos4608@gmail.com>
2020-05-20 00:23:08 +0200

    Updates

---
+++ precache-manifest.8fab7c0de91f13035c2807f51e438dcb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e29b39504dc3d16df1fe18eda2abc0f",
+    "url": "/Olios-app/index.html"
+  },
+  {
+    "revision": "4d446c14f0b26adcf202",
+    "url": "/Olios-app/static/js/2.5f4198e3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Olios-app/static/js/2.5f4198e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29a6832dd3cf03cab27c",
+    "url": "/Olios-app/static/js/main.bd4e2768.chunk.js"
+  },
+  {
+    "revision": "40e0be396e23f9e65e17",
+    "url": "/Olios-app/static/js/runtime-main.fb1a4826.js"
+  },
+  {
+    "revision": "8c01739c38dadb8ea2beb5e6037f5892",
+    "url": "/Olios-app/static/media/background.8c01739c.jpg"
+  }
+]);
